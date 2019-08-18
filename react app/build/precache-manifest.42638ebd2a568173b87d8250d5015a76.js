f65d5d69772be3229543557b5a5079192dafb623
Aidan White <aidan.white36@gmail.com>
2019-08-18 12:03:14 -0500

    Add build folder

---
+++ react app/build/precache-manifest.42638ebd2a568173b87d8250d5015a76.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41c6b90005f2744bce9c35d66ba07d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74d9d36cd91201607615",
+    "url": "/static/js/2.38c17154.chunk.js"
+  },
+  {
+    "revision": "f83d09317bdbf6d78602",
+    "url": "/static/js/main.a4bf56f7.chunk.js"
+  },
+  {
+    "revision": "0fb7fab12186f6b5d4cd",
+    "url": "/static/js/runtime~main.80f009fa.js"
+  }
+]);
