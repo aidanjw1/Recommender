c6ca200a2e1877dd20ace8518c86e6281fbef01a
Aidan White <aidan.white36@gmail.com>
2019-08-25 22:48:34 -0500

    Fix mobile bug

--- react app/src/Components/AutoComplete.js
+++ react app/src/Components/AutoComplete.js
@@ -9,8 +9,11 @@ const AutoComplete = ({data, onSelect}) => {
     data = text.length < 3? [] : data.filter(item => item.title.toLowerCase().includes(text.toLowerCase())).slice(0, 10)
 
     function handleClickAway() {
-        setText('');
-        setFocus(false);
+        // Hacky way of making it work on mobile
+        setTimeout(() => {
+            setText('');
+            setFocus(false)
+        },500);
     }
     function handleClickOn() {
         setFocus(true);
@@ -18,13 +21,14 @@ const AutoComplete = ({data, onSelect}) => {
 
     return (
         <div style={{ width: '50%', margin: 'auto'}}>
-            <ClickAwayListener onClickAway={handleClickAway}>
+            {/* <ClickAwayListener onClickAway={handleClickAway}> */}
                 <Input type="text" placeholder={'Search...'}
                     style={{width: '100%', fontSize: '1.5rem'}}
                     value={text} 
                     onChange={(e) => setText(e.target.value)} 
-                    onFocus={handleClickOn} />
-            </ClickAwayListener>
+                    onFocus={handleClickOn}
+                    onBlur={handleClickAway} />
+            {/* </ClickAwayListener> */}
             <Paper style={{position: 'absolute', display: focus? 'block' : 'none', zIndex: 1000 }}>
                 <List>
                     {data.map((val, i) => (
