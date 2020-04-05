fa574158482d97bf8a7b37f69a893efff27466d5
Aidan White <aidan.white36@gmail.com>
2020-04-05 12:18:34 -0500

    Fix movie card bug

--- react-app/src/Components/MovieBox.js
+++ react-app/src/Components/MovieBox.js
@@ -1,32 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
-import { Card, CardContent, Button, CardActions } from "@material-ui/core";
+import { Card, CardContent, Button } from "@material-ui/core";
 import { TMDB_KEY } from "../config";
   
 
 const MovieBox = ({ movie, remove }) => {
+    console.log(movie);
 
-    const [ src, setSrc ] = useState('');
+    const [ movieDetails, setMovieDetails ] = useState({});
 
     useEffect(() => {
-        async function fetchPoster() {
+        async function fetchMovie() {
             const res = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${TMDB_KEY}&language=en-US`);
             const body = await res.json();
-            setSrc(body['poster_path']);
+            setMovieDetails(body);
         }
-        fetchPoster();
-    })
+        fetchMovie();
+    }, [movie]);
 
     return (
         
             <Card style={{display: 'flex', width: '100%'}}>
                 <Link to={`/movie/${movie}`}>
-                    <img src={`https://image.tmdb.org/t/p/original/${src}`}
+                    <img src={`https://image.tmdb.org/t/p/original/${movieDetails['poster_path']}`} alt={movieDetails['poster_path']}
                         height={130} width={100} />
                 </Link>
                 <CardContent>
-                    <Link style={{width: '100%'}} to={`/movie/${movie.id}`}>
-                        {movie.title} <br/>
+                    <Link style={{width: '100%'}} to={`/movie/${movie}`}>
+                        {movieDetails.title} <br/>
                     </Link>
                     {remove? <Button onClick={remove} size={'small'} style={{backgroundColor: 'red', color: 'white', margin: '1rem'}}>Remove</Button> : null }                        
                 </CardContent>         
